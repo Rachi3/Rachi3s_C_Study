8bd7a8020bb444f19b56b074944fc8c057f625ed
Rachi3 <rlagusdn715@gmail.com>
2023-09-26 21:20:25 +0900

    이전 챕터4 연산자 실습

---
+++ ch.4 연산자/ch.4 연산자/secToHour.c
@@ -0,0 +1,24 @@
+#pragma warning(disable:4996)
+
+#include<stdio.h>
+
+int main(void)
+{
+	/*int sec = 0, min = 0, hour = 0, temp = 0, x;
+
+	scanf("%d", &temp);
+	x = temp;
+
+	hour = temp / 3600;
+	temp %= 3600;
+
+	min = temp / 60;
+	sec = temp % 60;
+
+	printf("%d 초는 %d시간 %d분 %d초 입니다\n",x, hour, min, sec);*/
+	
+	
+	printf("%d", 202022200202020.0f == 202022200202040.0f);
+
+	return 0;
+}
